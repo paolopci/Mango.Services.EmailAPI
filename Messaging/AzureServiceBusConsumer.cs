8d085613fc740fba747e73d14c0d06b263809847
PaoloPci <paolopci@yahoo.it>
2024-02-27 10:19:15 +0100

    003 Register Methods to Processor

--- Messaging/AzureServiceBusConsumer.cs
+++ Messaging/AzureServiceBusConsumer.cs
@@ -1,14 +1,18 @@
-﻿using Azure.Messaging.ServiceBus;
+﻿using System.Text;
+using Azure.Messaging.ServiceBus;
+using Mango.Services.EmailAPI.Models.Dto;
+using Newtonsoft.Json;
+
 
 namespace Mango.Services.EmailAPI.Messaging
 {
-    public class AzureServiceBusConsumer
+    public class AzureServiceBusConsumer : IAzureServiceBusConsumer
     {
         private readonly string serviceBusConnectionString;
         private readonly string emailCartQueue;
         private IConfiguration _configuration;
 
-        private ServiceBusProcessor _emailBusProcessor;
+        private ServiceBusProcessor _emailCartProcessor;
 
         public AzureServiceBusConsumer(IConfiguration configuration)
         {
@@ -18,7 +22,45 @@ namespace Mango.Services.EmailAPI.Messaging
 
 
             var client = new ServiceBusClient(serviceBusConnectionString);
-            _emailBusProcessor = client.CreateProcessor(emailCartQueue);
+            _emailCartProcessor = client.CreateProcessor(emailCartQueue);
+        }
+
+        public async Task Start()
+        {
+            _emailCartProcessor.ProcessMessageAsync += OnEmailCartRequestReceived;
+            _emailCartProcessor.ProcessErrorAsync += ErrorHandler;
+        }
+
+        private async Task OnEmailCartRequestReceived(ProcessMessageEventArgs args)
+        {
+            // this is where you will receive message
+            var message = args.Message;
+            // nel body del messaggio su azure che il messaggio che è CartDto serializzato.
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            CartDto objMessage = JsonConvert.DeserializeObject<CartDto>(body);
+
+            try
+            {
+                // TODO - try to log email
+                await args.CompleteMessageAsync(args.Message);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        private Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        public async Task Stop()
+        {
+            await _emailCartProcessor.StopProcessingAsync();
+            await _emailCartProcessor.DisposeAsync();
         }
     }
 }
